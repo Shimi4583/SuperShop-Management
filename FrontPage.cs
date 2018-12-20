2d7b60573f0a3ed3b149bf88aad9d87ab25a4b1c
Suraiya Rahman Shimi <33289115+Shimi4583@users.noreply.github.com>
2018-12-20 12:30:04 +0600

    Add files via upload

---
+++ FrontPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperShopManagement
+{
+    public partial class FrontPage : Form
+    {
+        public FrontPage()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox2.Text);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
