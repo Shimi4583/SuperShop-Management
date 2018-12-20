7a861f3f89542ec9b7f9c1ecb159652b866b1ce6
Suraiya Rahman Shimi <33289115+Shimi4583@users.noreply.github.com>
2018-12-20 12:30:57 +0600

    Add files via upload

---
+++ OrderDeatils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SuperShopManagement
+{
+    public partial class OrderDeatils : Form
+    {
+        public OrderDeatils()
+        {
+            InitializeComponent();
+        }
+
+        private void OrderDeatils_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
